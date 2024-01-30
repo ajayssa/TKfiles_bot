4cadd4960441d43d746ed6a67404c650683a376b
ajayssa <141111302+ajayssa@users.noreply.github.com>
2024-01-30 12:49:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/AutoFilterAdvance /AutoFilterAdvance
+  git clone https://github.com/ajayssa/TKfiles_bot /AutoFilterAdvance
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AutoFilterAdvance
+  git clone $UPSTREAM_REPO /https://github.com/ajayssa/TKfiles_bot
 fi
 cd /AutoFilterAdvance
 pip3 install -U -r requirements.txt
